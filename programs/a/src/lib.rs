92b7b6222f9e03759b82d9923b2a51da5272c060
Sourya07 <singhsourya137@gmail.com>
2026-02-12 22:15:38 +0530

    added the files

---
+++ programs/a/src/lib.rs
@@ -0,0 +1,32 @@
+use anchor_lang::prelude::*;
+ 
+declare_id!("11111111111111111111111111111111");
+ 
+#[program]
+mod hello_anchor {
+    use super::*;
+    pub fn initialize(ctx: Context<Initialize>, data: u64) -> Result<()> {
+        ctx.accounts.new_account.data = data;
+        msg!("Changed data to: {}!", data);
+        Ok(())
+    }
+}
+ 
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = signer, space = 8 + 8)]
+    pub new_account: Account<'info, NewAccount>,
+    pub old_account : Account<'info,OldAccount>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+ 
+#[account]
+pub struct NewAccount {
+    data: u64,
+}
+#[account]
+pub struct OldAccount {
+    data: u64,
+}
